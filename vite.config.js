962e8c41060f51d71c43ac70fedfbb68d48d4aad
Angelica Naguio <angelica.naguio@avvoka.com>
2026-08-02 03:12:23 +0800

    Split Kulintang startup bundle

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ export default defineConfig({
   build: {
     target: "es2022",
     sourcemap: true,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "three",
+              test: /[\\/]node_modules[\\/]three[\\/]/,
+              priority: 20,
+            },
+          ],
+        },
+      },
+    },
   },
 });
